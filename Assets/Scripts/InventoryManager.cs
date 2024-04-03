fca9c218170bbe007e5f8636bc0a5e98c36cb644
hamdibenhmida <benhmidahamdi3@gmail.com>
2024-04-03 06:13:39 +0100

    Revert "Revert "inventory update""
    This reverts commit 7a6781f3b4a576fadbb69e5fca513582f65ad61c.

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,26 +1,26 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
-using static UnityEditor.Experimental.GraphView.Port;
+using static UnityEditor.Progress;
+
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance { get; set; }
 
     public GameObject[] slots;
+
+    public GameObject activeItemSlot ;//{ get; private set; }
+    public GameObject availableItemSlot ;//{ get; private set; }
+
+    
     
 
+    public Transform FPSCamera;
+    public float dropForwardForce;
+    public float dropUpwardForce;
 
-    public GameObject activeItemSlot = null;
-    public GameObject availableItemSlot = null;
-    
-
-    public int i = 1;
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -35,8 +35,8 @@ public class InventoryManager : MonoBehaviour
 
     private void Start()
     {
-        activeItemSlot = slots[0];
-        availableItemSlot = slots[0];
+        
+        
        
     }
 
@@ -52,29 +52,28 @@ public class InventoryManager : MonoBehaviour
         #region inputs 
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            switchActiveSlot(0);
-            UIManager.Instance.switchInventoryActiveUISlot(0);
+            SetActiveSlot(0);
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            switchActiveSlot(1);
-            UIManager.Instance.switchInventoryActiveUISlot(1);
+            SetActiveSlot(1);
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            switchActiveSlot(2);
-            UIManager.Instance.switchInventoryActiveUISlot(2);
+            SetActiveSlot(2);
         }
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            switchActiveSlot(3);
-            UIManager.Instance.switchInventoryActiveUISlot(3);
+            SetActiveSlot(3);
         }
         if (Input.GetKeyDown(KeyCode.Alpha5))
         {
-            switchActiveSlot(4);
-            UIManager.Instance.switchInventoryActiveUISlot(4);
-        } 
+            SetActiveSlot(4);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            DropItem();
+        }
         #endregion 
 
 
@@ -95,15 +94,19 @@ public class InventoryManager : MonoBehaviour
         }
     }
 
-    private GameObject GetAvailableSlot()
+    private void GetAvailableSlot()
     {
-        while (availableItemSlot.transform.childCount != 0 && i < slots.Length)
+        
+
+        for (int i = 0; i < slots.Length; i++)
         {
-            availableItemSlot = slots[i];
-            i++;
+            if (slots[i].transform.childCount == 0) 
+            { 
+                availableItemSlot = slots[i];
+                break;
+            }
         }
         
-        return availableItemSlot;
     }
 
     public void pickUpItem(GameObject pickedUpItem)
@@ -116,8 +119,22 @@ public class InventoryManager : MonoBehaviour
         pickedUpItem.transform.SetParent(availableItemSlot.transform,false);
 
         item item = pickedUpItem.GetComponent<item>();
-        
-        pickedUpItem.transform.GetComponent<BoxCollider>().enabled = false; //need to change the collider component later
+        // make collider istrigger
+        Collider itemCollider = pickedUpItem.GetComponent<Collider>();
+        if (itemCollider != null)
+        {
+            itemCollider.isTrigger = true;
+        }
+
+        // Disable the Rigidbody's gravity and set kinematic to true
+        Rigidbody itemRigidbody = pickedUpItem.GetComponent<Rigidbody>();
+        if (itemRigidbody != null)
+        {
+            
+            itemRigidbody.isKinematic = true;
+        }
+
+
         pickedUpItem.transform.localPosition = new Vector3(item.spawnPosition.x , item.spawnPosition.y,item.spawnPosition.z);
         pickedUpItem.transform.localRotation = Quaternion.Euler(item.spawnRotation.x, item.spawnRotation.y,item.spawnRotation.z);
 
@@ -127,20 +144,74 @@ public class InventoryManager : MonoBehaviour
         
     }
 
-    private void switchActiveSlot(int slotNumber)
+   
+    private void SetActiveSlot(int slotNumber)
     {
-        if(activeItemSlot.transform.childCount > 0)
+
+        if (slots[slotNumber].transform.childCount > 0)
         {
-            item currentItem = activeItemSlot.transform.GetChild(0).GetComponent<item>();
-            currentItem.isActiveItem = false;
+            
+            if (activeItemSlot != null && activeItemSlot.transform.childCount > 0)
+            {
+                item currentItem = activeItemSlot.transform.GetChild(0).GetComponent<item>();
+                currentItem.isActiveItem = false;
+            }
+
+            if (activeItemSlot == slots[slotNumber]) // Check if the selected slot is already active
+            {
+                activeItemSlot = null; // Deactivate the slot
+                UIManager.Instance.switchInventoryActiveUISlot(-1); // Pass -1 to indicate no active slot
+                return;
+            }
+
+            activeItemSlot = slots[slotNumber];
+            UIManager.Instance.switchInventoryActiveUISlot(slotNumber);
+
+            if (activeItemSlot.transform.childCount > 0)
+            {
+                item newItem = activeItemSlot.transform.GetChild(0).GetComponent<item>();
+                newItem.isActiveItem = true;
+            } 
         }
 
-        activeItemSlot = slots[slotNumber];   
-
-        if(activeItemSlot.transform.childCount > 0)
-        {
-            item newItem = activeItemSlot.transform.GetChild(0).GetComponent<item>();
-            newItem.isActiveItem = true;
-        }
     }
+
+    private void DropItem()
+    {
+        
+        if (activeItemSlot != null && activeItemSlot.transform.childCount > 0)
+        {
+
+            item currentItem = activeItemSlot.transform.GetChild(0).GetComponent<item>();
+            currentItem.transform.SetParent(null);
+            currentItem.isActiveItem = false;
+
+            // Reset the activeItemSlot and update the UI to reflect no active slot
+            activeItemSlot = null;
+            UIManager.Instance.updateInventoryItemIcon(null); // Pass null to indicate no item is being held
+
+            Rigidbody itemRigidbody = currentItem.GetComponent<Rigidbody>();
+            if (itemRigidbody != null)
+            {
+                // Add force to throw the item forward
+                currentItem.GetComponent<BoxCollider>().isTrigger = false;// reset collider to normal
+                itemRigidbody.isKinematic = false; // Ensure the item's Rigidbody is not kinematic
+
+                itemRigidbody.AddForce(FPSCamera.forward * dropForwardForce, ForceMode.Impulse);
+                itemRigidbody.AddForce(FPSCamera.up * dropUpwardForce, ForceMode.Impulse);
+            }
+            else
+            {
+                Debug.LogWarning("Rigidbody component not found on the item.");
+            }
+            
+        }
+        
+        
+
+
+
+    }
+
+
 }
